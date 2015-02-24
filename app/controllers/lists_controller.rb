b866793965e6495c1307a630136b1c8910d328a5
Bryan Roth <bryan@bryanroth.com>
2015-02-24 01:51:25 -0600

    added ability to create and view lists

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,23 @@
+class ListsController < ApplicationController
+	def new
+	end
+	
+	def index
+		@lists = List.all
+	end
+
+	def create
+		@list = List.new(list_params)
+		@list.save
+		redirect_to @list
+	end
+
+	def show
+		@list = List.find(params[:id])
+	end
+
+	private
+		def list_params
+			params.require(:list).permit(:title)
+		end
+end
